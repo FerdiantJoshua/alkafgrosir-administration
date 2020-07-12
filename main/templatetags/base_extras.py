d984943686f0622ca1a4d2c0666339df4145d408
FerdiantJoshua <ferdiant.98@gmail.com>
2020-07-13 05:34:11 +0700

    Add get_purchase_queryset_str_repr filter in base_extras.py

--- main/templatetags/base_extras.py
+++ main/templatetags/base_extras.py
@@ -1,4 +1,8 @@
 from django import template
+from django.db.models import QuerySet
+from django.utils.translation import gettext as _
+
+from transaction.models import Purchase
 
 register = template.Library()
 
@@ -15,3 +19,14 @@ def add_placeholder(form_field):
     placeholder = ' '.join(list(map(lambda x: x[0].upper() + x[1:], form_field.name.split('_'))))
     form_field.field.widget.attrs['placeholder'] = placeholder
     return form_field
+
+
+@register.filter()
+def get_purchase_queryset_str_repr(value):
+    if not isinstance(value, QuerySet) or value.model is not Purchase:
+        raise ValueError(_(f'The get_purchase_queryset_str_repr filter only accept Purchase QuerySet!'))
+    print(value)
+    purchases = []
+    for purchase in value:
+        purchases.append(f'{purchase.product.name} ({purchase.product.color}) = {purchase.amount}')
+    return ',\n'.join(purchases)
