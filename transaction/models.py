13a56efc7b77a184b949fa6679461e7aff509f3b
FerdiantJoshua <ferdiant.98@gmail.com>
2020-07-17 14:32:31 +0700

    Update model, migrations file, and tests

--- transaction/models.py
+++ transaction/models.py
@@ -1,14 +1,19 @@
+from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.db import models
-from django.urls import reverse
+from django.db.models import Q
+from django.urls import reverse, NoReverseMatch
+
 
 
 class City(models.Model):
     name = models.CharField(
-        'City Name', max_length=24,
-        primary_key=True, unique=True,
-        validators=[RegexValidator('^[A-Z_]*$', 'Only uppercase letters and underscores allowed.')]
+        'City Name', max_length=24, unique=True,
+        validators=[RegexValidator('^[A-Z \.]*$', 'Only uppercase letters, space, and period are allowed.')]
     )
+
+    def get_absolute_url(self):
+        return reverse('management:edit_city', args=[self.pk])
 
     def __str__(self):
         return self.name
@@ -18,12 +23,15 @@ class Customer(models.Model):
     name = models.CharField('Customer Name', max_length=48)
     username = models.CharField('Username', max_length=48, unique=True, blank=True, null=True)
 
+    def get_absolute_url(self):
+        return reverse('management:edit_customer', args=[self.pk])
+
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    code = models.CharField('Product Code', max_length=64, unique=True, primary_key=True)
+    code = models.CharField('Product Code', max_length=64, unique=True)
     name = models.CharField('Product Name', max_length=48)
     color = models.CharField('Product Color', max_length=48)
     size = models.CharField('Product Size', max_length=24)
@@ -32,8 +40,11 @@ class Product(models.Model):
     class Meta:
         unique_together = (('name', 'color', 'size'), )
 
+    def get_absolute_url(self):
+        return reverse('management:edit_product', args=[self.pk])
+
     def __str__(self):
-        return f'{self.code} ({self.name}-{self.size}-{self.color}) = {self.stock}'
+        return f'{self.name} {self.color} {self.size} ({self.code}), {self.stock} remaining'
 
 
 class Marketplace(models.Model):
@@ -51,8 +62,33 @@ class Courier(models.Model):
     name = models.CharField('Marketplace Name', max_length=24, unique=True)
     short_name = models.CharField('Short Name', max_length=5, unique=True)
 
+    def get_absolute_url(self):
+        return reverse('management:edit_courier', args=[self.pk])
+
     def __str__(self):
         return self.name
+
+
+class TransactionQuerySet(models.QuerySet):
+    def search_by_criteria(self, criteria):
+        transactions = self.filter(
+            Q(date__gte=criteria['date_start']) &
+            Q(date__lte=criteria['date_end']) &
+            Q(marketplace__name__icontains=criteria.get('marketplace', '')) &
+            Q(customer__name__icontains=criteria.get('customer', '')) &
+            Q(city__name__icontains=criteria.get('city', '')) &
+            Q(purchase__product__code__icontains=criteria.get('purchases', '')) |
+            Q(purchase__product__name__icontains=criteria.get('purchases', '')) |
+            Q(purchase__product__color__icontains=criteria.get('purchases', '')) |
+            Q(purchase__product__size__icontains=criteria.get('purchases', '')) &
+            Q(courier__name__icontains=criteria.get('courier', ''))
+        ).distinct().order_by('date', 'number')
+        if criteria.get('is_prepared'):
+            transactions = transactions.filter(is_prepared=criteria['is_prepared'])
+        if criteria.get('is_packed'):
+            transactions = transactions.filter(is_packed=criteria['is_packed'])
+
+        return transactions
 
 
 class Transaction(models.Model):
@@ -62,8 +98,24 @@ class Transaction(models.Model):
     customer = models.ForeignKey(Customer, verbose_name='Customer', on_delete=models.SET_NULL, null=True)
     city = models.ForeignKey(City, verbose_name='City', on_delete=models.SET_NULL, null=True)
     courier = models.ForeignKey(Courier, verbose_name='Courier', on_delete=models.SET_NULL, null=True)
+    packager = models.ForeignKey(User, verbose_name='Packager', on_delete=models.SET_NULL, blank=True, null=True)
     is_prepared = models.BooleanField('Is Prepared', default=False)
     is_packed = models.BooleanField('Is Packed', default=False)
+
+    objects = TransactionQuerySet.as_manager()
+
+    @staticmethod
+    def get_absolute_attribute_helptext_urls():
+        try:
+            absolute_attribute_helptext_urls = {
+                'marketplace': reverse('management:create_marketplace'),
+                'customer': reverse('management:create_customer'),
+                'city': reverse('management:create_city'),
+                'courier': reverse('management:create_courier')
+            }
+        except NoReverseMatch as e:
+            raise ValueError(f'Problematic get_absolute_attribute_helptext_urls function in {__class__} .') from e
+        return absolute_attribute_helptext_urls
 
     def get_absolute_url(self):
         return reverse('transaction:edit_transaction', args=[self.pk])
@@ -77,6 +129,8 @@ class Purchase(models.Model):
     product = models.ForeignKey(Product, verbose_name='Product', help_text='Start typing to get product choices',
                                 on_delete=models.SET_NULL, null=True)
     amount = models.PositiveIntegerField('Amount', default=1, null=False)
+    additional_information = models.CharField('Additional Information', max_length=128, blank=True, null=False)
 
     def __str__(self):
-        return f'{self.transaction}-{self.product.name}({self.product.color}) -> {self.amount}'
+        return f'{self.transaction}-{self.product.name}({self.product.color}) -> {self.amount}' \
+                + f' ({self.additional_information})' if {self.additional_information} else ''
--- transaction/tests.py
+++ transaction/tests.py
@@ -6,23 +6,42 @@ from .models import City, Customer, Product, Marketplace, Courier, Transaction, 
 
 class ModelsTest(TestCase):
     def test_model_creation(self):
-        city_01 = City.objects.get_or_create(name='BANDUNG')[0]
-        city_02 = City.objects.get_or_create(name='JAKARTA')[0]
-        customer_01 = Customer.objects.get_or_create(name='Budi', username='budi')[0]
-        customer_02 = Customer.objects.get_or_create(name='Anto', username='anto')[0]
-        product_01 = Product.objects.get_or_create(code='S1108.HD1674.17', name='HD1674', color='Biru Baby', size='30x70')[0]
-        product_02 = Product.objects.get_or_create(code='S1108.HD1674.03', name='HD1674', color='Pink Baby', size='30x70')[0]
-        marketplace_01 = Marketplace.objects.get_or_create(name='Shopee', short_name='S')[0]
-        marketplace_02 = Marketplace.objects.get_or_create(name='Tokopedia', short_name='T')[0]
-        courier_01 = Courier.objects.get_or_create(name='JNE', short_name='JNE')[0]
-        courier_02 = Courier.objects.get_or_create(name='JNT', short_name='JNT')[0]
+        cities = []
+        customers = []
+        products = []
+        marketplaces = []
+        couriers = []
+        transactions = []
+        purchases = []
+        
+        cities.append(City.objects.get_or_create(name='BANDUNG')[0])
+        cities.append(City.objects.get_or_create(name='JAKARTA')[0])
+        cities.append(City.objects.get_or_create(name='SURABAYA')[0])
+        customers.append(Customer.objects.get_or_create(name='Budi', username='budi')[0])
+        customers.append(Customer.objects.get_or_create(name='Anto', username='anto')[0])
+        customers.append(Customer.objects.get_or_create(name='Mawar', username='mawar')[0])
+        products.append(Product.objects.get_or_create(code='S1108.HD1674.17', name='HD1674', color='Biru Baby', size='30x70')[0])
+        products.append(Product.objects.get_or_create(code='S1108.HD1674.03', name='HD1674', color='Pink Baby', size='30x70')[0])
+        products.append(Product.objects.get_or_create(code='S1105.HD122-25', name='HD122', color='Putih', size='30x70')[0])
+        marketplaces.append(Marketplace.objects.get_or_create(name='Shopee', short_name='S')[0])
+        marketplaces.append(Marketplace.objects.get_or_create(name='Tokopedia', short_name='T')[0])
+        marketplaces.append(Marketplace.objects.get_or_create(name='Bukalapak', short_name='BL')[0])
+        couriers.append(Courier.objects.get_or_create(name='JNE', short_name='JNE')[0])
+        couriers.append(Courier.objects.get_or_create(name='JNT', short_name='JNT')[0])
+        couriers.append(Courier.objects.get_or_create(name='SiCepat', short_name='SC')[0])
 
-        transaction_01 = Transaction.objects.get_or_create(number=1, date=timezone.now(),
-                                  marketplace=marketplace_01, customer=customer_01, city=city_01, courier=courier_01)[0]
-        transaction_02 = Transaction.objects.get_or_create(number=2, date=timezone.now(),
-                                  marketplace=marketplace_02, customer=customer_02, city=city_02, courier=courier_02)[0]
-        purchase_01 = Purchase.objects.get_or_create(transaction=transaction_01, product=product_01, amount=8)[0]
-        purchase_02 = Purchase.objects.get_or_create(transaction=transaction_02, product=product_02, amount=10)[0]
+        transactions.append(Transaction.objects.get_or_create(number=1, date=timezone.now(), marketplace=marketplaces[0], 
+                                                            customer=customers[0], city=cities[0], courier=couriers[0])[0])
+        transactions.append(Transaction.objects.get_or_create(number=2, date=timezone.now(), marketplace=marketplaces[1],
+                                                              customer=customers[1], city=cities[1], courier=couriers[1])[0])
+        transactions.append(Transaction.objects.get_or_create(number=2, date=timezone.now(), marketplace=marketplaces[2],
+                                                              customer=customers[2], city=cities[2], courier=couriers[2])[0])
+        purchases.append(Purchase.objects.get_or_create(transaction=transactions[0], product=products[0], amount=8)[0])
+        purchases.append(Purchase.objects.get_or_create(transaction=transactions[1], product=products[1], amount=10)[0])
+        purchases.append(Purchase.objects.get_or_create(transaction=transactions[2], product=products[0], amount=12)[0])
+        purchases.append(Purchase.objects.get_or_create(transaction=transactions[2], product=products[1], amount=12)[0])
+        purchases.append(Purchase.objects.get_or_create(transaction=transactions[2], product=products[2], amount=12)[0])
 
-        self.assertEqual(transaction_01.purchase_set.get(transaction=transaction_01), purchase_01)
-        self.assertEqual(transaction_02.purchase_set.get(transaction=transaction_02), purchase_02)
+        self.assertEqual(transactions[0].purchase_set.get(transaction=transactions[0]), purchases[0])
+        self.assertEqual(transactions[1].purchase_set.get(transaction=transactions[1]), purchases[1])
+        self.assertEqual(len(transactions[2].purchase_set.all()), 3)
