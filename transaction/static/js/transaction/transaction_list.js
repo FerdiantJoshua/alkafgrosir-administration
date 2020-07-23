8b9101523318776656c722abb7faf758e2fb603f
FerdiantJoshua <ferdiant.98@gmail.com>
2020-07-23 07:31:59 +0700

    Add column visibility button in transaction_list.html

--- transaction/static/js/transaction/transaction_list.js
+++ transaction/static/js/transaction/transaction_list.js
@@ -1,15 +1,48 @@
 String.prototype.format = function() {
   a = this;
   for (k in arguments) {
-    a = a.replace("{" + k + "}", arguments[k])
+    a = a.replace('{' + k + '}', arguments[k])
   }
   return a
 }
 
-$(document).ready(function() {
-  $('.purchase-detail-button').each((key, value) => {
-    value.addEventListener('click', () => {
-      var id = value.parentElement.id.split('_').pop()
+function initializeColumnVisibility(colVisButtonIdPrefix, colTargetClass, columnHideData) {
+  for (let i = 0; i < columnHideData.length; i++) {
+    id = columnHideData[i]
+    const colVisButton = $(colVisButtonIdPrefix + id)
+    const colTargets = $(colTargetClass + id)
+    colVisButton.removeClass('active')
+    colTargets.addClass('d-none')
+  }
+}
+
+function setColumnVisibilityDropdownMenuItemOnClick(colVisButtons, colTargetClass, columnHideData) {
+  colVisButtons.each((idx, colVisButton) => {
+    colVisButton = $(colVisButton)
+    colVisButton.on('click', (e) => {
+      e.preventDefault()
+      const target = colVisButton.attr('id').split('-').pop()
+      const colTargets = $(colTargetClass + target)
+      if (colVisButton.attr('class').split(' ').includes('active')) {
+        colVisButton.removeClass('active')
+        colTargets.addClass('d-none')
+        columnHideData.push(target)
+        window.name = JSON.stringify(columnHideData)
+      } else {
+        colVisButton.addClass('active')
+        colTargets.removeClass('d-none')
+        columnHideData.pop(target)
+        window.name = JSON.stringify(columnHideData)
+      }
+    })
+  })
+}
+
+function setPurchaseDetailButtonsOnClick(purchaseDetailButtons) {
+  purchaseDetailButtons.each((idx, button) => {
+    button = $(button)
+    button.click(() => {
+      var id = button.parent().attr('id').split('_').pop()
       var transactionDate = $('#transaction_date_' + id).text()
       var transactionNumber = $('#transaction_number_' + id).text()
       var transactionCustomer = $('#transaction_customer_' + id).text()
@@ -19,4 +52,17 @@ $(document).ready(function() {
       $('#purchaseDetailModalBody').html(($('#transaction_purchase_' + id).find('.d-none').html()))
     })
   })
+}
+
+$(document).ready(function() {
+  let columnHideData = window.name === '' ? [] : JSON.parse(window.name)
+  console.log(columnHideData)
+  let [colVisButtonClass, colVisButtonIdPrefix] = ['.col-vis-btn', '#col-vis-btn-']
+  let colVisButtons = $(colVisButtonClass)
+  let colTargetClass = '.col-'
+
+  initializeColumnVisibility(colVisButtonIdPrefix, colTargetClass, columnHideData)
+  setColumnVisibilityDropdownMenuItemOnClick(colVisButtons, colTargetClass, columnHideData)
+
+  setPurchaseDetailButtonsOnClick($('.purchase-detail-button'))
 })
